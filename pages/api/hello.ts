fec4248400203ac48c20ec1acf97fbce6fa7345a
Huy Dam <huy@tactable.io>
2021-03-22 16:58:53 -0400

    feat: custom authorization

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,12 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import {NextApiRequest, NextApiResponse} from "next";
-import {getSession, withApiAuthRequired} from "@auth0/nextjs-auth0";
+import {getAccessToken, getSession, withApiAuthRequired} from "@auth0/nextjs-auth0";
 
-const HelloRoute = (req: NextApiRequest, res: NextApiResponse) => {
+const HelloRoute = async (req: NextApiRequest, res: NextApiResponse) => {
   const session = getSession(req, res)
+  const {accessToken} = await getAccessToken(req, res)
   res.status(200).json({ message: `Hello ${session?.user?.name} 
-    \n Here is your session data: ${JSON.stringify(session)}` })
+    \n Here is your session data: ${JSON.stringify(session)}. \n 
+    Access Token is: ${accessToken}` })
 }
 
 export default withApiAuthRequired(HelloRoute)
